80ed3449e4ce5e4f11100ac2cc2c30301fd4a3ba
Web-CODED <progeng94@gmail.com>
2023-11-14 12:00:53 +0530

    Add files via upload

---
+++ src/components/Top/Top.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Top.css';
+
+function Top() {
+    return(
+        <div className="container d-none" id="top">
+            <div id="top-phone" className="hov">
+                <ion-icon name="call-outline"></ion-icon>
+                <p>0800800800</p>
+            </div>
+            <div id="top-other"  className="hov">
+                <ion-icon name="logo-facebook"></ion-icon>
+                <ion-icon name="mail-outline"></ion-icon>
+                <ion-icon name="location-outline"></ion-icon>
+            </div>
+        </div>
+    )
+}
+
+export default Top;
