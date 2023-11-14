0d059814fad5edca799674f92e489932c11696ee
Web-CODED <progeng94@gmail.com>
2023-11-14 11:46:35 +0530

    Add files via upload

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import Top from '../Top/Top';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
+function Layout(props) {
+    return(
+        <div>
+            <Top />
+            <Header />
+                { props.children }
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
