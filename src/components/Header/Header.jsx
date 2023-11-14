3a15228ee61ac48eb79303d8b261a8f9f951efff
Web-CODED <progeng94@gmail.com>
2023-11-14 11:42:49 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+function Header() {
+    return(
+        <div id="navbar">
+            <Navbar className='fixed-top' bg="warning" variant='light' expand="lg">
+            <Container>
+                <Link className="text-dark text-decoration-none" id="logo" to="/">Korlake</Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className='justify-content-end my-2 my-lg-0' id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link className="nav-link active text-uppercase my-1 my-lg-0" to="/">Home</Link>
+                        <Link className="nav-link text-uppercase my-1 my-lg-0" to="/menu">Menu</Link>
+                        <Link className="nav-link text-uppercase my-1 my-lg-0" to="/story">Story</Link>
+                        <div className='mx-lg-2 my-2 my-lg-0'>
+                            <Button className='text-uppercase my-1 my-lg-0' variant='outline-dark' href="#contact">Book a table</Button>
+                        </div>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Header;
