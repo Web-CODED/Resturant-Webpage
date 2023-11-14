e31fccd96ea5d5b502fc66f3201f46e5d5faf67b
Web-CODED <progeng94@gmail.com>
2023-11-14 11:41:19 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return(
+        <div className="bg-warning p-3 text-center" id="footer">
+            &copy; made by Ionut Cora, 2021-2023
+        </div>
+    )
+}
+
+export default Footer;
